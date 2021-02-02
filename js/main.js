5eb754e49fb4b0f9e60251e57b4fea4ae469dcf1
Юрий <turbofan2000@mail.ru>
2021-02-02 15:01:26 +0300

    Убрал ошибки

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ const sumWay1 = function (firstNumber, secondNumber) {
   return firstNumber + secondNumber;
 }
 
-console.log(sumWay1(1, 3));
+sumWay1(1, 3);
 
 // функция сложения - декларирование
 function sumWay2 (firstNumber, secondNumber) {
   return Number(firstNumber) + Number(secondNumber);
 }
 
-console.log(sumWay2('123', 5));
+sumWay2('123', 5);
 
 // обязательные функции из ДЗ "2. Нужно больше функций"
 // функция случайное целое число из диапазона
@@ -18,11 +18,10 @@ const getRandom = function (minNumber, maxNumber) {
   if (minNumber <= maxNumber) {
     return Math.ceil(Math.random() * (maxNumber - minNumber) + minNumber);
   }
-  console.log('Диапазон чисел отрицательный!');
   return false;
 }
 
-console.log(getRandom(5, 10));
+getRandom(5, 10);
 
 // функция случайное число из диапазона с заданным "количеством знаков после запятой"
 const getRandomDecimalPlaces = function (minNumber, maxNumber, decimalPlaces) {
@@ -33,4 +32,4 @@ const getRandomDecimalPlaces = function (minNumber, maxNumber, decimalPlaces) {
   return false;
 }
 
-console.log(getRandomDecimalPlaces(0, 20, 3));
+getRandomDecimalPlaces(0, 20, 3);
