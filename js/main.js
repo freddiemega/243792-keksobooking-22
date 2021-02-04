0637bd3e36e2006a0be4e196251e026ea84c4b9f
Юрий <turbofan2000@mail.ru>
2021-02-04 13:34:27 +0300

    Исправлено дублирование кода

--- js/main.js
+++ js/main.js
@@ -15,16 +15,12 @@ sumWay2('123', 5);
 // обязательные функции из ДЗ "2. Нужно больше функций"
 // функция случайное целое число из диапазона
 const getRandom = function (minNumber, maxNumber) {
-  if (minNumber <= maxNumber) {
-    return Math.ceil(Math.random() * (maxNumber - minNumber) + minNumber);
-  } else {
-    //если аргументы перепутаны, то функция всё равно сработает - переставит аргументы местами
-    return Math.ceil(Math.random() * (minNumber - maxNumber) + maxNumber);
-  }
-
+  const min = Math.min(minNumber, maxNumber);
+  const max = Math.max(minNumber, maxNumber);
+  return Math.ceil(Math.random() * (max - min) + min);
 }
 
-getRandom(5, 10);
+getRandom(1, 10);
 
 // функция случайное число из диапазона с заданным "количеством знаков после запятой"
 const getRandomDecimalPlaces = function (minNumber, maxNumber, decimalPlaces) {
